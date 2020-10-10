22c904b3a0bd13eddb8d157c309e0db6fca77fb7
Gabriele Fahrer <gabrielemf0325@gmail.com>
2020-10-10 15:27:22 +0200

    app.module changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppFormComponent } from './volunteer/app-form/app-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { AppFormComponent } from './volunteer/app-form/app-form.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
