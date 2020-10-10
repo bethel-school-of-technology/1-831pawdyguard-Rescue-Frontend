5191310046479d026e7929007761443f7d13107b
Gabriele Fahrer <gabrielemf0325@gmail.com>
2020-10-10 16:28:31 +0200

    inital navbar added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppFormComponent } from './volunteer/app-form/app-form.component';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 
 import { DonationsComponent } from './donate/donations/donations.component';
+import { HeaderComponent } from './header/header.component';
+import { MatToolbarModule } from "@angular/material/toolbar";
 
 
 @NgModule({
   declarations: [
     AppComponent,
     AppFormComponent,
-    DonationsComponent
+    DonationsComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule, MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
