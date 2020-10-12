0904aca0132386031b362733e44ea831bdc1c2b8
Gabriele Fahrer <gabrielemf0325@gmail.com>
2020-10-12 09:50:18 +0200

    app form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AppFormComponent } from './volunteer/app-form/app-form.component';
 
-
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 import { DonationsComponent } from './donate/donations/donations.component';
 import { HeaderComponent } from './header/header.component';
-import { MatToolbarModule } from "@angular/material/toolbar";
+
 
 
 @NgModule({
@@ -23,7 +25,7 @@ import { MatToolbarModule } from "@angular/material/toolbar";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule, MatToolbarModule
+    BrowserAnimationsModule, MatToolbarModule, MatInputModule, MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
