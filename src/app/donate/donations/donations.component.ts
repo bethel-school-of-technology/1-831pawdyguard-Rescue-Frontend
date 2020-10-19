9c7ce93e5d6c7fcfc37906be0aa69cf9d48e89f3
complete-beep <57547792+complete-beep@users.noreply.github.com>
2020-10-19 08:29:51 -0500

    donations form added

--- src/app/donate/donations/donations.component.ts
+++ src/app/donate/donations/donations.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-donations',
   templateUrl: './donations.component.html',
   styleUrls: ['./donations.component.css']
-})
-export class DonationsComponent implements OnInit {
+ })
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class DonationsComponent {
 
 }
+
+// export class DonationsComponent implements OnInit {
+//   donations = {
+//   content: '',
+//   image: 'assets/images/pitter.jpg'
+
