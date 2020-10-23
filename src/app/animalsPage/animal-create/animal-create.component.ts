7b8a93c0f1646f31c7881bdf50edf9795941a132
MichelleBachini <michellejbachini@gmail.com>
2020-10-22 21:51:54 -0700

    modified all animal files for routing

--- src/app/animalsPage/animal-create/animal-create.component.ts
+++ src/app/animalsPage/animal-create/animal-create.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 
 import { AnimalsService } from '../animals.service';
 import { Animal } from '../animal.model';
+import { mediaType } from './media-type.validate';
 
 @Component({
   selector: 'app-animal-create',
@@ -14,18 +15,42 @@ export class AnimalCreateComponent implements OnInit {
   enteredTitle = '';
   enteredContent = '';
   animal: Animal;
+  form: FormGroup;
+  imagePreview: string;
   private mode = 'create';
   private animalId: string;
 
   constructor(public animalsService: AnimalsService, public route: ActivatedRoute) {}
 
   ngOnInit() {
+    this.form = new FormGroup({
+      title: new FormControl(null, {
+        validators: [Validators.required, Validators.minLength(3)],
+      }),
+      content: new FormControl(null, {
+        validators: [Validators.required],
+      }),
+      image: new FormControl(null, {
+        validators: [Validators.required],
+        asyncValidators: [mediaType],
+      }),
+    });
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
       if (paramMap.has('animalId')) {
         this.mode = 'edit';
         this.animalId = paramMap.get('animalId');
         this.animalsService.getAnimal(this.animalId).subscribe(animalData => {
-          this.animal = {id: animalData._id, title: animalData.title, content: animalData.content};
+          this.animal = {
+            id: animalData._id,
+            title: animalData.title,
+            content: animalData.content,
+            imagePath: animalData.imagePath
+          };
+          this.form.setValue({
+            title: this.animal.title,
+            content: this.animal.content,
+            image: this.animal.imagePath,
+          });
         });
       } else {
         this.mode = 'create';
@@ -34,16 +59,26 @@ export class AnimalCreateComponent implements OnInit {
     });
   }
 
-  onSaveAnimal(form: NgForm) {
-    if (form.invalid) {
+  onImagePicked(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.form.patchValue({ image: file });
+    this.form.get('image').updateValueAndValidity();
+    const reader = new FileReader();
+    reader.onload = () => {
+      this.imagePreview = reader.result as string;
+    };
+    reader.readAsDataURL(file);
+  }
+
+  onSaveAnimal() {
+    if (this.form.invalid) {
       return;
     }
     if (this.mode === 'create') {
-      this.animalsService.addAnimal(form.value.title, form.value.content);
+      this.animalsService.addAnimal(this.form.value.title, this.form.value.content, this.form.value.image);
     } else {
-      this.animalsService.updateAnimal(this.animalId, form.value.title, form.value.content);
+      this.animalsService.updateAnimal(this.animalId, this.form.value.title, this.form.value.content, this.form.value.image);
     }
-
-    form.resetForm();
+    this.form.reset();
   }
 }
--- src/app/animalsPage/animal.model.ts
+++ src/app/animalsPage/animal.model.ts
@@ -2,4 +2,5 @@ export interface Animal {
   id: string;
   title: string;
   content: string;
+  imagePath: string;
 }
