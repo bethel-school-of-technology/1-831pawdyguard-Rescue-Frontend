62448f941c9e3225fbf3c2c04b284285d3b9bf67
MichelleBachini <michellejbachini@gmail.com>
2020-10-23 21:47:17 -0700

    added code to ani-create.comp, cleaned up code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,16 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from "@angular/common/http";
-
+import { AngularMaterialModule } from './material.module';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from './app-routing.module';
 
-import { AngularMaterialModule } from './material.module';
+import { AnimalsModule } from './animalsPage/animals.module';
 
 import { AppFormComponent } from './volunteer/app-form/app-form.component';
 import { DonationsFormComponent } from './donate/donations-form/donations-form.component';
-
 import { DonationsComponent } from './donate/donations/donations.component';
 
 import { HomeComponent } from './home/home.component';
@@ -21,8 +20,6 @@ import { FooterComponent } from './footer/footer.component';
 import { LoginComponent} from './auth/login/login.component';
 import { VolMainComponent } from './volunteer/vol-main/vol-main.component';
 import { SignupComponent } from './auth/signup/signup.component';
-import { AnimalsModule } from './animalsPage/animals.module';
-
 
 @NgModule({
   declarations: [
