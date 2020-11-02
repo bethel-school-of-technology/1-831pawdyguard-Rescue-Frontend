1feab11844b4ccd2305d05749039cf6cd6eb16cb
MichelleBachini <michellejbachini@gmail.com>
2020-11-01 21:51:10 -0800

    changed Animals folder, routing

---
+++ src/app/animals/animals.module.ts
@@ -0,0 +1,23 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
+import { AnimalCreateComponent } from './animal-create/animal-create.component';
+import { AnimalListComponent } from './animal-list/animal-list.component';
+import { AngularMaterialModule } from '../material.module';
+
+@NgModule({
+  declarations: [AnimalCreateComponent, AnimalListComponent],
+  imports: [
+    CommonModule,
+    // NgModule,
+    ReactiveFormsModule,
+    FormsModule,
+    AngularMaterialModule,
+    RouterModule
+  ]
+})
+export class AnimalsModule {}
+
+
