83975ed82e90b478656ee98f0c7225fcc4683066
MichelleBachini <michellejbachini@gmail.com>
2020-11-12 19:58:59 -0800

    added global variable backend_url

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -30,8 +30,7 @@ import { MatCarouselModule } from '@ngmodule/material-carousel';
 
 import { LayoutComponent } from './layout/layout.component';
 
-import { AuthModule } from './auth/auth.module';
-import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';   
+import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
 import { AnimalAdoptionComponent } from './animals/animal-adopt/animal-adopt.component';
 
 @NgModule({
